bfdcd46d0f141a048b6077919f553832e225ab7b
Tarik Gadoumi <tarik_gadoumi@hotmail.fr>
2025-11-06 12:24:14 +0100

    chore(settings): increase  token life span

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
